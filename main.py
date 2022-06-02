db449b66f2bf55ba5c444bd643f5b1588ab488e7
Almaz Kunpeissov <almaz.kun@gmail.com>
2022-06-02 12:49:21 +0900

    Qr code with the image

--- main.py
+++ main.py
@@ -1,6 +1,37 @@
+import qrcode
+
+from PIL import Image
+
+logo_path = "logo.png"
+
+logo = Image.open(logo_path)
+
+basewidth = 100
+
+wpercent = basewidth / float(logo.size[0])
+
+hsize = int((float(logo.size[1]) * float(wpercent)))
+
+logo = logo.resize((basewidth, hsize), Image.ANTIALIAS)
+
+QRcode = qrcode.QRCode(error_correction=qrcode.constants.ERROR_CORRECT_H)
+
+url = "https://github.com/almazkun"
+
+QRcode.add_data(url)
+
+QRimg = QRcode.make_image(back_color="white").convert("RGB")
+
+QRimg = QRcode.make_image(
+    back_color="white").convert('RGB')
+
+pos = ((QRimg.size[0] - logo.size[0]) // 2,
+       (QRimg.size[1] - logo.size[1]) // 2)
+
+QRimg.paste(logo, pos)
+
 def main():
-    print("Hello World!")
-    return True
+    QRimg.save("qr.png")
 
 
 if __name__ == "__main__":
